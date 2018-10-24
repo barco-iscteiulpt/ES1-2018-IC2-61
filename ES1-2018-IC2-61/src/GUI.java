d7bc828f524e4a403e253f31d4fd26f39d774401
Bruno Colaço <barco@iscte-iul.pt>
2018-10-24 22:29:45 +0100

    Comments on initialize() method
    - Added some comments on the initialize() method to further clarify each
    section.

--- ES1-2018-IC2-61/src/GUI.java
+++ ES1-2018-IC2-61/src/GUI.java
@@ -50,7 +50,7 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import javax.swing.JComboBox;
 
-public class GUI extends Thread{
+public class GUI extends Thread {
 
 	private JFrame frame;
 	private JTextField keywords;
@@ -85,19 +85,22 @@ public class GUI extends Thread{
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
-		// Create the frame. Specify the title, placement, size, closing operation and layout of the frame.
+
+		// Create the frame. Specify the title, placement, size, closing operation and
+		// layout of the frame.
 		frame = new JFrame();
 		frame.setTitle("Bom Dia Academia!");
 		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+
+		// Create the menu bar. Create and add 3 menus.
 		JMenuBar menuBar = new JMenuBar();
-		JMenu menu1 = new JMenu ("File");
-		JMenu menu2 = new JMenu ("Edit");
-		JMenu menu3 = new JMenu ("About");
-	
+		JMenu menu1 = new JMenu("File");
+		JMenu menu2 = new JMenu("Edit");
+		JMenu menu3 = new JMenu("About");
+
+		// Create menu items and add them to their respective menu.
 		JMenuItem item1 = new JMenuItem("Close");
 		JMenuItem item2 = new JMenuItem("New");
 		menu1.add(item2);
@@ -105,81 +108,85 @@ public class GUI extends Thread{
 		menuBar.add(menu1);
 		menuBar.add(menu2);
 		menuBar.add(menu3);
-		
+
+		// Add ActionListeners to all menu items.
 		item1.addActionListener(new ActionListener() {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				frame.dispose();	
+				frame.dispose();
 			}
 		});
-		
+
 		item2.addActionListener(new ActionListener() {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				main(null);	
+				main(null);
 			}
 		});
+
 		frame.setJMenuBar(menuBar);
-		
-		// Create the top panel. Specify height and layout of the panel.  Add it to the frame.
+
+		// Create the top panel. Specify height and layout of the panel. Add it to the
+		// frame.
 		JPanel top = new JPanel();
 		top.setLayout(new GridLayout(1, 2, 0, 0));
 		frame.getContentPane().add(top, BorderLayout.NORTH);
-		
-		// Create the left side of the top panel. Specify layout and orientation. Add to top panel.
+
+		// Create the left side of the top panel. Specify layout and orientation. Add to
+		// top panel.
 		JPanel top_left = new JPanel();
 		top_left.setLayout(new FlowLayout(FlowLayout.LEADING));
 		top.add(top_left);
-		
-		// Get social icons from "resources" folder. Create 3 checkboxes (selected by default). Add them to top-left panel.
+
+		// Get social icons from "resources" folder. Create 3 checkboxes (selected by
+		// default). Add them to top-left panel.
 		JLabel fb_icon = new JLabel(new ImageIcon("src/resources/facebook.png"));
 		JLabel twitter_icon = new JLabel(new ImageIcon("src/resources/twitter.png"));
 		JLabel email_icon = new JLabel(new ImageIcon("src/resources/email.png"));
-		
+
 		JCheckBox fb_checkbox = new JCheckBox();
 		fb_checkbox.setSelected(true);
 		JCheckBox twitter_checkbox = new JCheckBox();
 		twitter_checkbox.setSelected(true);
 		JCheckBox email_checkbox = new JCheckBox();
 		email_checkbox.setSelected(true);
-		
+
 		top_left.add(fb_checkbox);
 		top_left.add(fb_icon);
 		top_left.add(twitter_checkbox);
 		top_left.add(twitter_icon);
 		top_left.add(email_checkbox);
 		top_left.add(email_icon);
-		
-		// Create the right side of the top panel. Specify layout and orientation. Add to top panel.
+
+		// Create the right side of the top panel. Specify layout and orientation. Add
+		// to top panel.
 		JPanel top_right = new JPanel();
 		top_right.setLayout(new FlowLayout(FlowLayout.TRAILING));
 		top.add(top_right);
-		
+
 		// Create period filter and add different options. Add to top-right panel.
-		String[] options = {"última hora","últimas 24h","último dia","última semana", "último mês"};
+		String[] options = { "última hora", "últimas 24h", "último dia", "última semana", "último mês" };
 		JComboBox comboBox = new JComboBox(options);
 		comboBox.setRenderer(new MyComboBoxRenderer("Defina um período..."));
 		comboBox.setSelectedIndex(-1);
 		top_right.add(comboBox);
-		
-		// Define keyword search and create "Search" button. Specify dimensions, margins and icon. Add to top-right panel.
+
+		// Define keyword search and create "Search" button. Specify dimensions, margins
+		// and icon. Add ActionListener. Add to top-right panel.
 		keywords = new JTextField();
 		JButton searchButton = new JButton();
-		
-		keywords.setPreferredSize(new Dimension(120,24));
-		searchButton.setMargin(new Insets(2,8,2,8));
+
+		keywords.setPreferredSize(new Dimension(120, 24));
+		searchButton.setMargin(new Insets(2, 8, 2, 8));
 		searchButton.setIcon(new ImageIcon("src/resources/magnifier-tool.png"));
-		
+
 		keywords.addActionListener(new ActionListener() {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				searchButton.doClick();
-				
 			}
 		});
+
 		top_right.add(keywords);
 		top_right.add(searchButton);
 
@@ -188,20 +195,23 @@ public class GUI extends Thread{
 		center.setLayout(new GridLayout(1, 2, 0, 0));
 		frame.getContentPane().add(center, BorderLayout.CENTER);
 
-		// Create timeline and article boxes. Add to center panel.
+		// Create scrolling panes. Create timeline and article boxes. Add them to the
+		// respective panes.
 		listModel = new DefaultListModel();
 		postList = new ArrayList<>();
-		
+
 		JScrollPane scrollPaneTimeline = new JScrollPane();
 		JScrollPane scrollPaneArticle = new JScrollPane();
 		JList<?> timeline = new JList(listModel);
 		scrollPaneTimeline.setViewportView(timeline);
-		
+
 		JTextArea article = new JTextArea();
 		scrollPaneArticle.setViewportView(article);
 		article.setLineWrap(true);
 		article.setWrapStyleWord(true);
-		
+
+		// Add ListSelectionListener to timeline and ActionListener to the search
+		// button.
 		timeline.addListSelectionListener(new ListSelectionListener() {
 			@Override
 			public void valueChanged(ListSelectionEvent e) {
@@ -209,79 +219,78 @@ public class GUI extends Thread{
 				article.setText(show.getMessage());
 			}
 		});
-		
+
 		center.add(scrollPaneTimeline);
 		center.add(article);
-		
-		searchButton.addActionListener(new ActionListener(){
+
+		searchButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (twitter_checkbox.isSelected())
 					searchTwitter(keywords.getText());
 				if (fb_checkbox.isSelected())
 					getFacebookData(keywords.getText());
-		}
-	});
+			}
+		});
 
 	}
-	
+
 	public void getFacebookData(String info) {
-		
+
 		FacebookClient fbClient = new DefaultFacebookClient(accessToken);
-		
+
 		Page page = fbClient.fetchObject("listajespeniche", Page.class);
-		
+
 		Connection<Post> postFeed = fbClient.fetchConnection(page.getId() + "/feed", Post.class);
-		
-		for(List<Post> postPage : postFeed) {
-			for(Post aPost : postPage) {
-				if(aPost.getMessage()!=null && aPost.getMessage().contains(info)) {
+
+		for (List<Post> postPage : postFeed) {
+			for (Post aPost : postPage) {
+				if (aPost.getMessage() != null && aPost.getMessage().contains(info)) {
 					postList.add(aPost);
-					System.out.println("-->"+aPost.getMessage());
-					
+					System.out.println("-->" + aPost.getMessage());
 				}
 			}
 		}
-		for(Post post : postList) {
-			String header = "Facebook: "+ post.getCreatedTime().toString() + "   " + post.getMessage().substring(0, 30)+"...";
-			listModel.addElement(header);	
+		for (Post post : postList) {
+			String header = "Facebook: " + post.getCreatedTime().toString() + "   " + post.getMessage().substring(0, 30)
+					+ "...";
+			listModel.addElement(header);
 		}
 	}
 
 	public void searchTwitter(String info) {
-		
+
 		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true)
-		.setOAuthConsumerKey("0I3XKOkznUjpuwdDOSkLvcSpg")
-		.setOAuthConsumerSecret("zy8i9meaxzK5Rn05rKIsJwWvclJPfdpmtfnE5UuJvHxsW6oZ0G")
-		.setOAuthAccessToken("325579017-itc5klbFYmBcGvHUaZaUz0sCD29J7GVfuMiw5ZCg")
-		.setOAuthAccessTokenSecret("Wz55x8BoTY8wdU5zwQCBI45520ic5JjLi9VCHXBArg5JT");
-		
+		cb.setDebugEnabled(true).setOAuthConsumerKey("0I3XKOkznUjpuwdDOSkLvcSpg")
+				.setOAuthConsumerSecret("zy8i9meaxzK5Rn05rKIsJwWvclJPfdpmtfnE5UuJvHxsW6oZ0G")
+				.setOAuthAccessToken("325579017-itc5klbFYmBcGvHUaZaUz0sCD29J7GVfuMiw5ZCg")
+				.setOAuthAccessTokenSecret("Wz55x8BoTY8wdU5zwQCBI45520ic5JjLi9VCHXBArg5JT");
+
 		TwitterFactory twitterFactory = new TwitterFactory(cb.build());
 		Twitter twitter = twitterFactory.getInstance();
-		
+
 		Query query = new Query("ISCTEIUL");
 		query.setCount(10);
 		QueryResult searchResult = null;
-			
-			try {
-				
-				searchResult = twitter.search(query);
-				
-				List<Status> tweetsList = searchResult.getTweets();
-				Iterator<Status> listIterator = tweetsList.iterator();
-				
-				while (listIterator.hasNext()) {
-					Status tweet = (Status)listIterator.next();
-					if(tweet.getText().contains(info)) {
-						System.out.println(tweet.getUser().getName() + ": " + tweet.getText());
-					}	
+
+		try {
+
+			searchResult = twitter.search(query);
+
+			List<Status> tweetsList = searchResult.getTweets();
+			Iterator<Status> listIterator = tweetsList.iterator();
+
+			while (listIterator.hasNext()) {
+				Status tweet = (Status) listIterator.next();
+				if (tweet.getText().contains(info)) {
+					System.out.println(tweet.getUser().getName() + ": " + tweet.getText());
 				}
-				
-			} catch (TwitterException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-			
+
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
+
 }
